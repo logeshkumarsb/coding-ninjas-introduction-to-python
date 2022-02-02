9f108f29ff913eb1eb2df26770a15c05a9621ea3
LOGESH KUMAR S <85162863+logeshkumarsb@users.noreply.github.com>
2022-02-02 20:47:56 +0530

    Create Triangle_Number_of_patterns.py

---
+++ pattern-II/Triangle_Number_of_patterns.py
@@ -0,0 +1,19 @@
+n=int(input())
+num = 1
+gap = n - 1
+for j in range(1, n + 1) :
+    num = j
+    for i in range(1, gap + 1) :
+        print(" ", end="")
+    gap = gap - 1
+         
+    for i in range(1, j + 1) :
+        print(num, end="")
+        num = num + 1
+     
+    num = num - 2
+    for i in range(1, j) :
+        print(num, end="")
+        num = num - 1
+     
+    print()
