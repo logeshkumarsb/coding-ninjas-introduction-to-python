7993f861f916db338329e15593970ee3feda392a
LOGESH KUMAR S <85162863+logeshkumarsb@users.noreply.github.com>
2022-02-02 20:51:41 +0530

    Update Arrow_patterm.py

--- pattern-II/Arrow_patterm.py
+++ pattern-II/Arrow_patterm.py
@@ -1,9 +1,17 @@
-num=int(input())
-for i in range(1,num+1):
-    for j in range(1,num-i+1):
-        print(end=" ")
-    for j in range(i,0,-1):
-        print(j,end="")
-    for j in range(2,i+1):
-        print(j,end="")
-    print()  
+n = int(input())
+n1 = (n+1)//2
+n2 = n-n1
+
+for i in range(1,n1+1):
+    for j in range(1,i):
+        print(' ',end='')
+    for k in range(1,i+1):
+        print('* ',end='')
+    print()    
+
+for i in range(n2,0,-1):
+    for j in range(1,i):
+        print(' ',end='')
+    for k in range(1,i+1):
+        print('* ',end='')
+    print()
