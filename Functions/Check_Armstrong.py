772e8006908effd90196d3b52e127348c3c96593
LOGESH KUMAR S <85162863+logeshkumarsb@users.noreply.github.com>
2022-02-21 00:12:49 +0530

    Create Check_Armstrong.py

---
+++ Functions/Check_Armstrong.py
@@ -0,0 +1,17 @@
+n=int(input())
+k=n
+j=n
+count=0
+while j!=0:
+    count=count+1   
+    j=j//10
+rev=0
+while k!=0:
+    a=k%10
+    j=a**count
+    rev=rev+j
+    k=k//10
+if(rev==n):
+    print("true")
+else:
+    print("false")
